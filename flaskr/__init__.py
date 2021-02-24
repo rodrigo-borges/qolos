fcb87c73ee71c410096a26fd547b1e99572b3007
Rodrigo Borges <rodrigo.sil.borges@gmail.com>
2021-02-24 18:44:21 -0300

    Change database path to JawsDB

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,7 +7,7 @@ def create_app(test_config=None):
 	app = Flask(__name__, instance_relative_config=True)
 	app.config.from_mapping(
 		SECRET_KEY="dev",
-		DATABASE="sqlite:///"+os.path.join(app.instance_path, "flaskr.sqlite")
+		DATABASE=os.environ["JAWSDB_URL"]
 	)
 
 	if test_config is None:
